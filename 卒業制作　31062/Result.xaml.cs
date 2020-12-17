3934c9c54a21eeb136eab544bee420591a4de3ff
IdaHibiki <31062@ojs.ac.jp>
2020-12-17 14:58:50 +0900

    プロジェクト ファイルを追加します。

---
+++ 卒業制作　31062/Result.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace 卒業制作_31062
+{
+    /// <summary>
+    /// Result.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Result : Window
+    {
+        public Result()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void btBackSelect_Click(object sender, RoutedEventArgs e)
+        {
+            Select sl = new Select();
+            sl.Show();
+            this.Close();
+        }
+    }
+}
---
+++ 卒業制作　31062/Select.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace 卒業制作_31062
+{
+    /// <summary>
+    /// Select.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Select : Window
+    {
+        public Select()
+        {
+            InitializeComponent();
+
+            cbBox.Items.Add("北海道(札幌市)");
+            cbBox.Items.Add("青森(青森市)");
+            cbBox.Items.Add("岩手(盛岡市)");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (cbBox.SelectedItem == null)
+            {
+                MessageBox.Show("都道府県を選択してください");   
+            }
+            else
+            {
+                Result rs = new Result();
+                rs.Show();
+                this.Close();
+            } 
+        }
+    }
+}
